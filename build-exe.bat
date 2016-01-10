5436104b31feb889f107aa8f152e54b5d21fdd37
Babaev Evgeniy <segrived@gmail.com>
2016-01-10 11:00:54 +0200

    Disable debug information generation when using ilrepack

--- build-exe.bat
+++ build-exe.bat
@@ -2,5 +2,6 @@ msbuild /p:Configuration=Release NSISInfoWriter.sln
 ilrepack /targetplatform:v4 ^
     /out:target\nsisiw.exe ^
     /parallel ^
+    /ndebug ^
     "%~dp0\NSISInfoWriter\bin\Release\nsisiw.exe" ^
     "%~dp0\NSISInfoWriter\bin\Release\CommandLine.dll"
